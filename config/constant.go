01301702d19fa63e957a5b91d99f533e0975f39b
Gtio <gtoxlili@outlook.com>
2025-11-03 23:42:35 +0800

    refactor: update series length and kline limit constants

--- config/constant.go
+++ config/constant.go
@@ -6,8 +6,8 @@ const (
 	KlineInterval       = 3 * time.Minute
 	KlineIntervalLonger = 4 * time.Hour
 
-	SeriesLength        = 10
-	KlineLimit          = 100
+	SeriesLength        = 16
+	KlineLimit          = 256
 	OiPeriod            = "5m"
 	OiLimit             = 288
 	MaxHistoricalValues = 1 >> 10 // 最多存储 1024 个历史账户总价值数据点
@@ -22,5 +22,5 @@ const (
 )
 
 var (
-	AssetUniverse = []string{"BTC", "ETH", "AERO", "BNB", "SOL"}
+	AssetUniverse = []string{"BTC", "ETH", "AERO", "BNB", "SOL", "XRP"}
 )
